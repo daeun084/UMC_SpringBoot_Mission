7cc55c4837592cd351063915a59e70c5355797f1
daeun084 <030804jk@naver.com>
2023-11-09 01:41:45 +0900

    first commit

---
+++ src/main/java/Umc/UMC_SpringBoot_Mission/UmcSpringBootMissionApplication.java
@@ -0,0 +1,13 @@
+package Umc.UMC_SpringBoot_Mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcSpringBootMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcSpringBootMissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Umc/UMC_SpringBoot_Mission/UmcSpringBootMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package Umc.UMC_SpringBoot_Mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcSpringBootMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
